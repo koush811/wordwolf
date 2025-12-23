571d0f1a8e332890f456eb856a19e8bb0e385979
test <koush0811@gmail.com>
2025-12-23 15:10:57 +0900

    a

--- api/test.js
+++ api/test.js
@@ -3,7 +3,7 @@ import handler from './generate-word.js';
 
 
 // モック req/res を作成
-const req = { method: "POST", body: { theme: "食べ物" } };
+const req = { method: "POST", body: { theme: "yukkuri-games" } };
 const res = {
   status: (code) => ({
     json: (obj) => console.log("status:", code, "response:", JSON.stringify(obj, null, 2))
